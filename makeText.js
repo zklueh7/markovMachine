1f4dcc885eb8a31166da7ebfa6bde51a375bc715
zoey klueh <zklueh@gmail.com>
2024-01-12 18:59:42 -0700

    complete markov exercise

--- makeText.js
+++ makeText.js
@@ -1,2 +1,43 @@
 /** Command-line tool to generate Markov text. */
 
+const axios = require('axios');
+const fs = require('fs');
+const process = require('process');
+const markov = require('./markov');
+
+function makeText(data) {
+    let markov = new markov.MarkovMachine(data);
+    console.log(markov);
+}
+
+function textFromFile(path) {
+    fs.readFile(path, 'utf8', (err,data) => {
+        if (err) {
+            console.log("ERROR", err);
+            process.kill(1);
+        }
+        generateText(data);
+    })
+}
+
+async function textFromURL(url) {
+    try {
+        let response = await axios.get(url);
+        generateText(response.data);
+    }
+    catch {
+        console.log("ERROR", err);
+        process.exit(1);
+    }
+}
+
+if (process.argv[2] == "file") {
+    textFromFile(process.argv[3]);
+}
+else if(process.argv[2] == "url") {
+    textFromURL(process.argv[3]);
+}
+else {
+    console.log("Invalid input");
+    process.exit(1);
+}
--- markov.js
+++ markov.js
@@ -17,13 +17,72 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = {};
+
+    this.words.forEach(function(value, index, arr) {
+
+      let word = value;
+      let nextWord = arr[index+1];
+
+      if (!(word in chains)) {
+        if(nextWord) {
+          chains[word] = [nextWord];
+        }
+        else {
+          chains[word] = [null];
+        }
+      }
+
+      else {
+        if(nextWord) {
+          chains[word].push(nextWord);
+        }
+        else {
+          chains[word].push(null);
+        }
+      }
+    })
+    
+    this.chains = chains;
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    let text = [];
+    let wordIdx, word, chainIdx, chainWord;
+    let count=numWords;
+    let state = "start";
+
+    for(let i=numWords; i>0; i--) {
+      if(state == "start") {
+        //choose new random word
+        wordIdx = Math.floor(Math.random() * this.words.length);
+        word = this.words[wordIdx];
+        text.push(word);
+        state = "chain";
+        count--;
+      }
+
+      else {
+        //choose random next word from chains for first word
+        chainIdx = Math.floor(Math.random() * this.chains[word].length);
+        chainWord = this.chains[word][chainIdx];
+        if(chainWord) {
+          word = chainWord;
+          text.push(chainWord);
+        }
+        else {
+          text.push(".");
+          state = "start";
+        }
+        count--;
+      }
+      
+    }
+    let output = text.join(' ');
+    console.log(output);
   }
 }
+ 
